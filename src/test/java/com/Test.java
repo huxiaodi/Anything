0fa68008f7ffd416010337c060717063b1e4157f
hudi <huxiaodi@qiatuchina.com>
2019-03-10 13:19:29 +0800

    数据连接

---
+++ src/test/java/com/Test.java
@@ -0,0 +1,17 @@
+package com;
+
+import com.test.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Description：
+ * @Author:hudi
+ * @Date: Created in 11:17 2019/3/8
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test {
+
+}
